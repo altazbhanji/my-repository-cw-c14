b8cb5945b582bfa3d6281dd6a16365fb40b58636
Altaz Bhanji <altazbhanji@gmail.com>
2023-05-22 20:49:40 -0400

    updated my name

--- 101-kittens-carousel-static-website-ec2/userdata.sh
+++ 101-kittens-carousel-static-website-ec2/userdata.sh
@@ -2,10 +2,11 @@
 yum update -y
 yum install httpd -y
 cd /var/www/html
-wget https://raw.githubusercontent.com/altazbhanji/my-repository/main/101-kittens-carousel-static-website-ec2/static-web/index.html
-wget https://raw.githubusercontent.com/altazbhanji/my-repository/main/101-kittens-carousel-static-website-ec2/static-web/cat0.jpg
-wget https://raw.githubusercontent.com/altazbhanji/my-repository/main/101-kittens-carousel-static-website-ec2/static-web/cat1.jpg
-wget https://raw.githubusercontent.com/altazbhanji/my-repository/main/101-kittens-carousel-static-website-ec2/static-web/cat2.jpg
+FOLDER="https://raw.githubusercontent.com/altazbhanji/my-repository/main/101-kittens-carousel-static-website-ec2/static-web"
+wget ${FOLDER}/index.html
+wget ${FOLDER}/cat0.jpg
+wget ${FOLDER}/cat1.jpg
+wget ${FOLDER}/cat2.jpg
 
 systemctl start httpd
 
