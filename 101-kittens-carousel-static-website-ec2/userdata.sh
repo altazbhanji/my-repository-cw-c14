4c3776427d18974ab00cf412d1c260181695c0ae
Altaz Bhanji <altazbhanji@gmail.com>
2023-06-26 20:32:54 -0400

    add phonebook app

--- 101-kittens-carousel-static-website-ec2/userdata.sh
+++ 101-kittens-carousel-static-website-ec2/userdata.sh
@@ -2,12 +2,10 @@
 yum update -y
 yum install httpd -y
 cd /var/www/html
-FOLDER="https://raw.githubusercontent.com/altazbhanji/my-repository/main/101-kittens-carousel-static-website-ec2/static-web"
-wget ${FOLDER}/index.html
-wget ${FOLDER}/cat0.jpg
-wget ${FOLDER}/cat1.jpg
-wget ${FOLDER}/cat2.jpg
-
+FOLDER="https://raw.githubusercontent.com/altazbhanji/my-repository/main/101-kittens-carousel-static-website-ec2/static-web/"
+wget ${FOLDER}index.html
+wget ${FOLDER}cat0.jpg
+wget ${FOLDER}cat1.jpg
+wget ${FOLDER}cat2.jpg
 systemctl start httpd
-
-
+systemctl enable httpd
